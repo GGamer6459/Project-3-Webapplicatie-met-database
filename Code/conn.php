b0cf5857dc3dba2ed573c391068dab24fc9748aa
GGamer6459 <9012644@student.zadkine.nl>
2021-03-08 17:05:08 +0100

    Create conn.php

---
+++ Code/conn.php
@@ -0,0 +1,15 @@
+<?php 
+$servername = "localhost";
+$dbname = "user";
+$username = "root";
+$password = "";
+ 
+try {
+    $conn = new PDO ("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Database verbonden!<br/>";
+}
+catch (PDOException $e) {
+    echo "Connectie mislukt.<br/>" . $e->getMessage();
+}
+?>
