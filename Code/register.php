bf775469e3cd24db9dad5a887cf97dbfb608f3e3
GGamer6459 <9012644@student.zadkine.nl>
2021-03-30 12:16:22 +0200

    Update register.php

--- Code/register.php
+++ Code/register.php
@@ -9,9 +9,9 @@
 
 <body>
     <form action="register_query.php" method="POST">
-        <label for="username">Gebruikersnaam</label>
+        <label for="username">Gebruikersnaam:</label>
         <input type="text" name="username" /><br>
-        <label for="password">Password</label>
+        <label for="password">Wachtwoord:</label>
         <input type="password" name="password" /><br>
         <br><button>Registreren</button>
     </form>
